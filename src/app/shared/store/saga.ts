45289dcd671c8a962e0f6ad2dc8a9074d36c806c
bro <bro@beam-mw.com>
2023-02-05 12:10:37 +0300

    naming

--- src/app/shared/store/saga.ts
+++ src/app/shared/store/saga.ts
@@ -16,7 +16,7 @@ import Utils from '@core/utils.js';
 export function remoteEventChannel() {
   return eventChannel((emitter) => {
     Utils.initialize({
-      "appname": "Beam Minter App",
+      "appname": "Beam Asset Minter App",
       "min_api_version": "7.2",
       "headless": false,
       "apiResultHandler": (error, result, full) => {
